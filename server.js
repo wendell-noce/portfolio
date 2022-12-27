e6f83d50c879ef92494fb228bdee27e6a835ba3a
wendell.noce <wendell.noce@gmail.com>
2022-12-27 18:53:48 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/dist/porfolio'));
+app.get('/*', (req,res) => {
+  res.sendFile(__dirname + 'dist/porfolio/index.html' );
+})
+app.listen(PORT, () => {
+  console.log('Servidor iniciado na porta ' + PORT)
+})
