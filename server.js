d539317a22fb770bbe80ea323f0520bf136ecb1e
wendell.noce <wendell.noce@gmail.com>
2022-12-27 19:12:48 -0300

    server js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const appName = 'portfolio';
 
 const PORT = process.env.PORT || 8080;
-app.use(express.static(__dirname + '/dist/porfolio'));
+app.use(express.static(`${__dirname}/dist/${appName}`));
+
 app.get('/*', (req,res) => {
-  res.sendFile(__dirname + 'dist/porfolio/index.html' );
+  res.sendFile(`${__dirname}/dist/${appName}/index.html`);
 })
+
 app.listen(PORT, () => {
   console.log('Servidor iniciado na porta ' + PORT)
 })
